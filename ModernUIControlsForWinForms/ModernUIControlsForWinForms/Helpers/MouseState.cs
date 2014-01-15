e9c8a23550e89d72dc6e1f6e5903a1db557e413c
Stefan Baumann <nafets-mail@freenet.de>
2014-01-15 12:39:16 +0100

    Added Helpers

---
+++ ModernUIControlsForWinForms/ModernUIControlsForWinForms/Helpers/MouseState.cs
@@ -0,0 +1,10 @@
+﻿namespace ModernUIControlsForWinForms.Helpers
+{
+    //Just created it, because I use Enums like this one all the time -> I don't have to create it over and over again.
+    public enum MouseState
+    {
+        Normal,
+        Hover,
+        MouseDown
+    }
+}
