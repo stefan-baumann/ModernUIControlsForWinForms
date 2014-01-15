bc9f517984892a75c3107881639e960bd989077f
Stefan Baumann <nafets-mail@freenet.de>
2014-01-15 21:20:21 +0100

    Edited ControlTestForm

--- ModernUIControlsForWinForms/ModernUIControlsForWinForms.Test/ControlTestForm.cs
+++ ModernUIControlsForWinForms/ModernUIControlsForWinForms.Test/ControlTestForm.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Windows.Forms;
 
 namespace ModernUIControlsForWinForms.Test
